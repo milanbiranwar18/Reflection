8b1378f01b850d83630840198b4154135abd4e0d
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 23:53:12 +0530

    Program for Reflection UC2

---
+++ MoodAnalyser1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionAssignment
+{
+    public class MoodAnalyser1
+    {
+        public string message;
+        public MoodAnalyser1(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser1()
+        {
+        }
+        public string analyseMood()
+        {
+
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("SAD"))
+                { return "SAD"; }
+                else
+                { return "HAPPY"; }
+            }
+            catch (NullReferenceException obj)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+}
