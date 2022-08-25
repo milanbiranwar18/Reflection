c30274b51c915f2f1792ac57a3a602e0a8704508
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 23:54:26 +0530

    Adding program for reflection UC2

--- Program.cs
+++ Program.cs
@@ -6,25 +6,57 @@ namespace ReflectionAssignment
     {
         public static void Main(string [] args)
         {
-           
 
-            Type type = typeof(MoodAnalyser);
 
-            Console.WriteLine("Class :" + type.Name);
+            //Type type = typeof(MoodAnalyser);
 
-            MethodInfo[] methods = type.GetMethods();
-            foreach (var method in methods)
+            //Console.WriteLine("Class :" + type.Name);
+
+            //MethodInfo[] methods = type.GetMethods();
+            //foreach (var method in methods)
+            //{
+            //    Console.WriteLine("Method : " + method.Name);
+            //    ParameterInfo[] parameters = method.GetParameters();
+            //    foreach (var p in parameters)
+            //    {
+            //        Console.WriteLine("Parameter : " + p.Name + " : " + p.ParameterType);
+            //    }
+            //}
+
+
+            //Type type1 = typeof(MoodAnalyser1);
+
+            //Console.WriteLine("Class :" + type1.Name);
+
+            //MethodInfo[] method = type1.GetMethods();
+            //foreach (var m in method)
+            //{
+            //    Console.WriteLine("Method : " + m.Name);
+            //    ParameterInfo[] parameters = m.GetParameters();
+            //    foreach (var p in parameters)
+            //    {
+            //        Console.WriteLine("Parameter : " + p.Name + " : " + p.ParameterType);
+            //    }
+            //}
+
+
+            Type type1 = typeof(MoodAnalyser1);
+            ConstructorInfo[] ci = type1.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+            foreach (ConstructorInfo ci2 in ci)
             {
-                Console.WriteLine("Method : " + method.Name);
-                ParameterInfo[] parameters = method.GetParameters();
-                foreach (var p in parameters)
-                {
-                    Console.WriteLine("Parameter : " + p.Name + " : " + p.ParameterType);
-                }
+                Console.WriteLine(ci2);
+            }
+
+            Console.WriteLine("--------------------------");
+
+            Type type2 = typeof(MoodAnalyser1);
+            MethodInfo[] mi = type2.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            foreach (MethodInfo c in mi)
+            {
+                Console.WriteLine(c);
             }
 
 
-            
         }
     }
 }
