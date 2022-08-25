7bae2a133e5fc94ccb639c557380fe71e86c8f35
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 23:41:27 +0530

    Program for Reflection UC1

---
+++ MoodAnalyser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionAssignment
+{
+    public class MoodAnalyser
+    {
+        public string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public MoodAnalyser()
+        {
+
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+
+                if (this.message.Contains("SAD"))
+                {
+                    return "SAD";
+                }
+                else
+
+                {
+                    return "HAPPY";
+                }
+
+            }
+            catch
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
