685146ef4b05282c0605b0d941f21fa9d7f173bb
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 23:39:30 +0530

    Adding program for reflectionUC1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,33 @@
-﻿
+﻿using System.Reflection;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Wellcome to Reflection Problem");
+namespace ReflectionAssignment
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+           
+
+            Type type = typeof(MoodAnalyser);
+
+            Console.WriteLine("Class :" + type.Name);
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach (var method in methods)
+            {
+                Console.WriteLine("Method : " + method.Name);
+                ParameterInfo[] parameters = method.GetParameters();
+                foreach (var p in parameters)
+                {
+                    Console.WriteLine("Parameter : " + p.Name + " : " + p.ParameterType);
+                }
+            }
+
+
+            
+        }
+    }
+}
+
+
+
