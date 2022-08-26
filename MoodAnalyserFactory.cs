e650ea05954d8b78e01ebc993e9dd68213e93f27
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-26 09:30:10 +0530

    program for mood analyser factory

---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReflectionAssignment
+{
+    public class MoodAnalyserFactory
+    {
+        private string message;
+        public MoodAnalyserFactory(string message)
+        {
+            this.message = message;
+        }
+
+        // UC 4
+        public static object CreateMoodAnalyser(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                }
+
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+            }
+            return null;
+        }
+    }
+}
